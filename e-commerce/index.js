ab598634b76a3dec4e9f9268d2665ed1cdc1c7c7
timiscode <timilehinolabisi15@gmail.com>
2026-09-21 14:19:13 +0100

    updated

--- e-commerce/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-
-// Bind to port to start server
-app.listen(process.env.PORT, () => {
-    console.log(`Express server started at port ${process.env.PORT}`);
-});
